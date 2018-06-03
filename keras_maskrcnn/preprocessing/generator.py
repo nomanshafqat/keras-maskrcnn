322ade8171b36306af7b1bed8557f199a3a449dd
Noman Shafqat <14besemshafqat@seecs.edu.pk>
2018-06-04 03:14:46 +0500

    undo

--- keras_maskrcnn/preprocessing/generator.py
+++ keras_maskrcnn/preprocessing/generator.py
@@ -125,7 +125,6 @@ class Generator(object):
 
             # randomly transform the masks and expand so to have a fake channel dimension
             for m in range(len(masks)):
-                masks[m] =np.expand_dims(masks[m], axis=-1)
                 masks[m] = apply_transform(transform, masks[m], self.transform_parameters)
                 masks[m] = np.expand_dims(masks[m], axis=2)
 
